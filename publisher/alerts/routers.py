b26b30c406632003c09a4ec27fc3138ad0353fe3
matheus-cortez <mrctcortez@gmail.com>
2023-07-12 22:34:44 -0300

    working on pub and sub connection with queues on rabbitmq

--- publisher/alerts/routers.py
+++ publisher/alerts/routers.py
@@ -5,6 +5,8 @@ from fastapi.responses import Response
 
 from publisher.alerts.schemas import AlertSchema
 
+import pika
+
 router_prefix: str = "/alerts"
 
 router_tag: str = "ALERTS"
@@ -23,6 +25,21 @@ router: APIRouter = APIRouter(
     response_description="Accepted",
     response_class=Response,
 )
+
 def create_alert(alert: AlertSchema) -> NoReturn:
-    # the publishing in queues must be done here
-    pass
+    # Connecting with RabbitMQ
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+
+    # Creating RabbitMQ exchange
+    channel.exchange_declare(exchange='weather_alert_exchange', exchange_type='direct')
+
+    channel.queue_declare(queue='YELLOW')
+    channel.queue_declare(queue='ORANGE')
+    channel.queue_declare(queue='RED')
+
+    channel.basic_publish(exchange='weather_alert_exchange',
+                      routing_key=alert.type,
+                      body='Publishing alert of type ' + alert.type)
+
+    connection.close()
--- subscriber/callbacks.py
+++ subscriber/callbacks.py
@@ -1,4 +1,19 @@
 from typing import NoReturn
+import pika
 
 def callback() -> NoReturn:
-    pass
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+
+    #channel.exchange_declare(exchange='logs', exchange_type='fanout')
+
+    channel.queue_declare(queue='YELLOW')
+    channel.queue_declare(queue='ORANGE')
+    channel.queue_declare(queue='RED')
+
+    # Set up a consumer for each queue
+    channel.basic_consume(queue='YELLOW', on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue='ORANGE', on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue='RED', on_message_callback=callback, auto_ack=True)
+
+    channel.start_consuming()
